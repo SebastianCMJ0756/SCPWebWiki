93c57ab532f5697296bb4af904c0af71883a6fec
SebastianCMJ0756 <juansebastiancalderonmartinez@gmail.com>
2026-08-05 16:42:47 -0500

    arreglo de gitpage#2

---
+++ src/app/scp/page.tsx
@@ -0,0 +1,8 @@
+export default function SCPIndexPage() {
+  return (
+    <main className="p-8 text-green-500 bg-black min-h-screen font-mono">
+      <h1 className="text-2xl font-bold mb-4">[ ACCESO A BASE DE DATOS SCP ]</h1>
+      <p>Seleccione un archivo de la lista para inspeccionar.</p>
+    </main>
+  );
+}
